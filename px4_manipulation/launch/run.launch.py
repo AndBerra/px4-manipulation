1e0adba9a6a1bc5b8fa902a3371e31095c07936f
AndBerra <andrea.berra@outlook.com>
2026-03-15 17:51:30 +0100

    launch directly visualize launch file

--- px4_manipulation/launch/run.launch.py
+++ px4_manipulation/launch/run.launch.py
@@ -1,5 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -9,30 +11,23 @@ def generate_launch_description():
         namespace='px4_manipulation',
         executable='talker',
         name='sim'
-    )
-    package_dir = get_package_share_directory('px4_offboard')
-    rviz = Node(
-        package='rviz2',
-        namespace='',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', [os.path.join(package_dir, 'visualize.rviz')]]
-    )
+    )    
     targetpose_marker = Node(
         package='px4_manipulation',
         namespace='px4_manipulation',
         executable='rviz_targetpose_marker.py',
         name='targetpose_marker'
     )
-    px4_visualizer = Node(
-        package='px4_offboard',
-        namespace='px4_offboard',
-        executable='visualizer',
-        name='visualizer'
+    px4_visualizer = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('px4_offboard'),
+                'visualize.launch.py'
+            )
+        )
     )
     return LaunchDescription([
         manipulation_node,
         px4_visualizer,
-        targetpose_marker,
-        rviz
+        targetpose_marker
 ])
